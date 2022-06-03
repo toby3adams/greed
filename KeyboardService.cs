3384f6ffd47f84504ab5fef07036384b9f675554
Tyler Brady <tjlego101@aol.com>
2022-06-03 17:33:08 -0600

    Added Keyboard Service Class and made small adjsutmetns to Object class Made Notes in Class

---
+++ KeyboardService.cs
@@ -0,0 +1,44 @@
+using Raylib_cs;
+using objects;
+
+namespace services
+{
+    public class KeyboardServices
+    {
+        private int ScaleingFactor = 10; // The Scaling Factor is a variable used to represent th size of the cell of the GUI
+        //This value is used so that when the player moves it will move faster as it will increase the amount of pixels moved
+        //by the player.
+        public KeyboardServices(int ScalelingFactor) { //** we may be able to use this to add and ability to increase speed.
+            this.ScaleingFactor = ScaleingFactor;
+        }
+
+        public objectProperties PlayerDirection() //Uses the principles of inheritance to call the Object class so that the returned 
+        //value will be initialized in the Object class
+        {
+            int XCoordinate = 0;
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) { //if the Right Arrow is pressed it will add a 1 to move the player right
+                XCoordinate = 1;
+            }
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) { //if the Left Arrow is pressed it will add a -1 to move the player left
+                XCoordinate = -1;
+            }
+
+            objectProperties movement = new objectProperties(XCoordinate,200,15); // I need to talk to Zak about Y-COORDS AND COLOR. This will initialize the XCoordinate for the Player Character
+            movement = movement.Factoring(ScaleingFactor); // New object initializes the values in the other class and then scales it and returns it
+            // we set the movement to the new factor because it is of the class type. So we can pass it back as the value wanted. 
+            return movement;
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
--- object.cs
+++ object.cs
@@ -1,6 +1,6 @@
 namespace objects
 {
-    class object
+    public class objectProperties //Tyler - I made it public so Keyboard Service can access your class
     {
         //attributes
         int location_x;
@@ -8,7 +8,7 @@ namespace objects
         int color;
 
         //constructor
-        public object(int location_x, int location_y, int color)
+        public objectProperties(int location_x, int location_y, int color)
         {
             this.location_x = location_x;
             this.location_y = location_y;
@@ -29,6 +29,18 @@ namespace objects
         public int getColor()
         {
             return color;
+        }
+
+
+        // TYLER ADDED THIS. this class allows me to pass your object class back into its self while increasing the speed of the player to update the players location. 
+        public objectProperties Factoring(int factor)// the factor is used to increase the speed of the player. Increase the amount of pixels moved.
+        {
+            int x = this.location_x * factor;
+            int y = this.location_y * factor;// we may need to set the y coord to a fixed point.
+    
+            return new objectProperties(x, y, 256); // We will need to talk about color and how I can implement it.
+            // we could make it so that as you move away from the center your color changes.
+            // do we know the center point? // Or if we touch a gem or rock the player will flash a color? 
         }
     }
 }
