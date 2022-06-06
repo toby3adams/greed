ae32f88257d04c4fdee8b8fee9ce4443ff198c2e
thussaiththelaw <thussaiththelaw@gmail.com>
2022-06-06 16:48:57 -0600

    fixed player and rocks constructor

--- Game/Objects/Player.cs
+++ Game/Objects/Player.cs
@@ -3,7 +3,7 @@ namespace greed.Game.Objects
     class player: objectProperties
     {
         //construct the player using initial values of location, color, and character
-        public player() : base(objectProperties)
+        public void playerconstructor()
         {
             objectconstructor(0, 0, 256, '#'); //Zak- starts the player at (0,0) !!!!!adjust this to the proper value when known!!!!!
         }
