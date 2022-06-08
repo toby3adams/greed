bdefc1c5e866d397d3af4b33c058a904fc77d6ce
Douglas Hill <drhill99@hotmail.com>
2022-06-08 16:38:18 -0600

    added initial code to Program.cs and current Director.cs

--- Game/Director.cs
+++ Game/Director.cs
@@ -1,39 +1,80 @@
-namespace Greed{
+using greed.Game.Objects;
+using greed.Game.Services;
+
+namespace Game{
 
     class Director{
+        private int cellSize = 15;
+        private string caption = "Greed";
+        private int width = 640;
+        private int height = 480;
+        private int frameRate = 0;
+        private bool debug = false;
+
         Random Random = new Random();
         private bool Playing = true;
-        public Director(){}
+        
+        public Director(){
+            KeyboardServices keyBoard = new KeyboardServices(cellSize);
+            VideoService videoService = new VideoService(caption, width, height, cellSize, frameRate, debug);   
+        }
 
         public void StartGame(){
+            
 
-            while(Playing){
-                
-                GetInputs();
-                DoUpdates();
-                DoOutputs();
+            Player player = new Player();
+            VideoService.OpenWindow();
+            while(VideoService.IsWindowOpen()){
 
+                GetInputs(cast);
+                DoUpdates(cast);
+                DoOutputs(cast);
 
             }        
         }
-        private void GetInputs(){
-            
+        private void GetInputs(Cast cast){
+            Actor robot = cast.GetFirstActor("robot");
+            Point velocity = keyboardService.GetDirection();
+            robot.SetVelocity(velocity);
         }
         private void DoUpdates(){
+            Actor banner = cast.GetFirstActor("banner");
+            Actor robot = cast.GetFirstActor("robot");
+            List<Actor> artifacts = cast.GetActors("artifacts");
+
+            banner.SetText("");
+            int maxX = videoService.GetWidth();
+            int maxY = videoService.GetHeight();
+            robot.MoveNext(maxX, maxY);
+
+            foreach (Actor actor in artifacts)
+            {
+                if (robot.GetPosition().Equals(actor.GetPosition()))
+                {
+                    Artifact artifact = (Artifact) actor;
+                    string message = artifact.GetMessage();
+                    banner.SetText(message);
+                }
+            } 
 
         }
         private void DoOutputs(){
-
+            List<Actor> actors = cast.GetAllActors();
+            VideoService.ClearBuffer();
+            VideoService.DrawActors(actors);
+            VideoService.FlushBuffer();
         }
-        private void Create_RG(){
-            CreateRock();
-            CreateGem();
-        }
+        
         private void CreateRock(){
+            Rock rock = new Rock();
 
         }
         private void CreateGem(){
+            Gem gem = new Gem();
 
+        }
+        private void init(){
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using greed.Game;
+using greed.Game.Objects;
+using greed.Game.Services;
+
+namespace greed{
+    class Program {
+        
+        static void Main(string[] args)
+        {
+            private int cellSize = 15;
+            private string caption = "Greed";
+            private int width = 640;
+            private int height = 480;
+            private int frameRate = 0;
+            private bool debug = false;
+            Cast cast = new Cast();
+            Director director = new Director();
+        }
+    }
+}
