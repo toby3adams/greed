f6c6e714d3160a56ed340790ed0016b8429e7e4b
thussaiththelaw <thussaiththelaw@gmail.com>
2022-06-08 10:58:16 -0600

    changed the name of object to actor, and changed names of methods to follow and changed each instance that it was called to follow

---
+++ Game/Objects/Actor.cs
@@ -0,0 +1,48 @@
+namespace greed.Game.Objects
+{
+
+    public class Actor //Tyler - I made it public so Keyboard Service can access your class
+    {
+        //attributes
+        int location_x;
+        int location_y;
+        int color;
+        char character;
+
+        //constructor
+        public void actorConstructor(int location_x, int location_y, int color, char character)
+        {
+            this.location_x = location_x;
+            this.location_y = location_y;
+            this.color = color;
+            this.character = character;
+        }
+        
+        //methods
+        public int getLocationX()
+        {
+            return this.location_x;
+        }
+        
+        public int getLocationY()
+        {
+            return this.location_y;
+        }
+
+        public int getColor()
+        {
+            return this.color;
+        }
+
+        public void shiftLocation(int x, int y)
+        {
+            this.location_x += x;
+            this.location_y += y;
+        }
+
+        public char getCharacter()
+        {
+            return this.character;
+        }
+    }
+}
--- Game/Objects/Gem.cs
+++ Game/Objects/Gem.cs
@@ -1,6 +1,6 @@
 namespace greed.Game.Objects
 {
-    class Gem : objectProperties
+    class Gem : Actor
     {
         //construct the player using initial values of location, color, and character
 
@@ -33,7 +33,7 @@ namespace greed.Game.Objects
 
 
 
-            objectconstructor(0, 0, RandColor, '*'); //Zak- Make sure to adjust the y location to the top of the screen.
+            actorConstructor(0, 0, RandColor, '*'); //Zak- Make sure to adjust the y location to the top of the screen.
             
             int x = random.Next(0, 10); //Zak- make sure to adjust the range to be within the bounds of the screen.
             shiftLocation(x, 0); //Zak-this will move the rock to the random x location
